dabc175b2735ece6d944452322719186952f68fa
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-09-10 12:42:53 +0530

    feat: add button.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.view}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>I want to share something ✍️</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: '#1976d3',
+    padding: 10,
+    borderRadius: 10
+  },
+  buttonText: {
+    color: 'white'
+  }
+})
